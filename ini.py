cc1f43f6c398525cdb095b6b6aa5c59ed4060291
Reza-XD <94230332+Reaper-XD@users.noreply.github.com>
2022-01-03 20:11:15 +0700

    Update and rename ppk/decode.py to ini.py

--- ini.py
+++ ini.py
@@ -4,7 +4,7 @@ import marshal, zlib, base64, os, sys
 try:
     r = sys.argv[1]
 except:
-    exit("Masukkan : python2 ....py CONTOH : python2 namafilenya.py")
+    exit("Masukkan : python2 ini....py CONTOH : python2 ini namafilenya.py")
 
 if not os.path.isfile(r):
     exit("File Tidak Ditemukan!")
