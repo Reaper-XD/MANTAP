579858b5f2280216dbb953dea3d83a37880440b9
Reza-XD <94230332+Reaper-XD@users.noreply.github.com>
2022-01-03 21:34:40 +0700

    Update ini.py

--- ini.py
+++ ini.py
@@ -1,13 +1,28 @@
 # module
-import marshal, zlib, base64, os, sys
-
+import marshal, zlib, base64, os, sys, time
+os.system("clear")
+os.system("git pull")
+REAPER = """
+╔═══╗╔═══╗╔═══╗╔═══╗╔═══╗╔═══╗────╔═══╗╔══╗╔╗───╔═══╗
+╚╗╔╗║║╔══╝║╔═╗║║╔═╗║╚╗╔╗║║╔══╝────║╔══╝╚╣─╝║║───║╔══╝
+─║║║║║╚══╗║║─╚╝║║─║║─║║║║║╚══╗────║╚══╗─║║─║║───║╚══╗
+─║║║║║╔══╝║║─╔╗║║─║║─║║║║║╔══╝────║╔══╝─║║─║║─╔╗║╔══╝
+╔╝╚╝║║╚══╗║╚═╝║║╚═╝║╔╝╚╝║║╚══╗────║║───╔╣─╗║╚═╝║║╚══╗
+╚═══╝╚═══╝╚═══╝╚═══╝╚═══╝╚═══╝────╚╝───╚══╝╚═══╝╚═══╝
+"""
+print REAPER
+def auto(z):
+    for e in z + "\n":
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.05)
 try:
     r = sys.argv[1]
 except:
-    exit("Masukkan : python2 ini....py CONTOH : python2 ini namafilenya.py")
+    auto("Masukkan : python2 ini....py CONTOH : python2 ini namafilenya.py")
 
 if not os.path.isfile(r):
-    exit("File Tidak Ditemukan!")
+    auto("File Tidak Ditemukan!")
 
 a = open(r).read()
 b = marshal.dumps(a)
@@ -17,4 +32,4 @@ d = base64.b64encode(c)
 sv = 'import marshal, zlib, base64\nexec(marshal.loads(zlib.decompress(base64.b64decode("{}"))))'
 
 open("ini_.py", "w").write(sv.format(d))
-exit("berhasil : file tersimpan ini_.py")
+auto("berhasil : file tersimpan ini_.py")
